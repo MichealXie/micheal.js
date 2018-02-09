3da2562b631136908c30f6cbe4153ebc1822ab5d
MichealXie <1441337380@qq.com>
2018-02-09 15:38:13 +0800

    修复 bug...

--- simple-promise.js
+++ simple-promise.js
@@ -3,14 +3,16 @@
 // 	return ???
 // }
 
-// var promise = new Promise(function (x, y) {
-// 	setTimeout(() => {
-// 		x(101)
-// 	}, 3000)
-// })
-// promise.then((z) => {
-// 	console.log(z)  // 101
-// })
+var promise = new Promise(function (x, y) {
+	setTimeout(() => {
+		// 把 101 传给了X, 也就是 resloved, 这就是为什么reslovedNotify能拿到 arguments
+		// 使用者需要 resloved 结果, 传递给 reslovedNotify, 全体 then 使用一个 arguments
+		x(101)
+	}, 1000)
+})
+promise.then((z) => {
+	console.log(z)  // 101
+})
 
 
 
@@ -21,20 +23,23 @@ function Promise(fn){
 	// 4. 执行结果保存在这里~
 	reslovedArray = [],
 	rejectedArray = [],
-	// 这俩函数的作用是执行 then 里面的代码
+	// 5.这俩函数的作用是执行 then 里面的代码
 	reslovedNotify = function () {
 		this.state = 'resloved'
-		reslovedArray.forEach(function(e){
-			e.call()
-		})
+		for (let e of reslovedArray){
+			e.apply(undefined, arguments)
+		}
+		// forEach 的话... this 会丢失
+		// reslovedArray.forEach((e) => {
+		// 	e.apply(undefined, arguments)
+		// })
 	}
 	rejectedNotify = function () {
 		this.state = 'rejected'
-		rejectedArray.forEach(function (e) {
-			e.call()
+		rejectedArray.forEach( (e) => {
+			e.apply(undefined, arguments)
 		})
 	}
-
 	// 3. 执行传入的函数, 成功调用reslovedNotify, 失败调用rejectedNotify, 通知 then 里的函数执行, 这俩函数我们自己要实现
 	fn.call(undefined, reslovedNotify, rejectedNotify)
 	// 2. 返回一个 thenable 对象, 把用户在 then 里传入的函数放到队列中
