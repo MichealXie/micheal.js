247396cc723b119bb8c1c2c7da90212836d60986
MichealXie <1441337380@qq.com>
2018-02-24 14:59:56 +0800

    深度 + 广度优先遍历 node 节点

--- traverseNodes.js
+++ traverseNodes.js
@@ -1,16 +1,23 @@
 function traverseNodes(selector){
-	let node = document.querySelector(selector)
 	let stack = []
-	console.log(node.tagName)
-	node.childNodes.forEach( (e, i) => {
-		stack.push(e)
-	})
+	stack.push(document.querySelector(selector))
 	while(stack.length){
-		let single = stack.shift()
-		if (single.tagName) console.log(single.tagName)
-		single.childNodes.forEach((e, i) => {
+		let node = stack.shift()
+		if (node.tagName) console.log(node.tagName)
+		node.childNodes.forEach((e, i) => {
 			stack.push(e)
 		})
 	}
 }
 traverseNodes('body')
+
+// 深度优先
+
+function deepTraverse(node){
+	node.childNodes.forEach( (e, i) => {
+		console.log(node.tagName)
+		if (e.childNodes.length) deepTraverse(e)
+	})
+}
+
+deepTraverse(document.querySelector('body'))
